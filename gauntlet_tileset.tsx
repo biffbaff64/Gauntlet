640c9f8600b0aa22249a1cd0d709a0815934683e
Richard Ikin / Red 7 Projects <richikin@gmail.com>
2022-02-04 22:44:26 +0000

    Add files via upload

---
+++ gauntlet_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="gauntlet_tileset" tilewidth="32" tileheight="32" tilecount="266" columns="19">
+ <image source="gauntlet_tileset.png" width="608" height="448"/>
+</tileset>
